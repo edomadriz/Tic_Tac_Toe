a6919d5a33ed35e5453f900707ec895cc6df9b7d
Gabriel Madriz Masís <edomadriz@gmail.com>
2014-02-05 10:40:27 -0600

    added the flow to make a movement in the board with basic validations

--- Game.rb
+++ Game.rb
@@ -6,17 +6,26 @@ class Game
   attr_reader :display
   def initialize
     @board = Board.new
-    @display = Display.new()
+    @display = Display.new
     @current_player = "X"
   end
 
-  def start_game2
+  def start_game
     display.start_game_message(@board.matrix)
     insert_move(display.get_player_move(@current_player))
   end
 
   def get_move(move)
-     valid_entry?(move) ? insert_move(move) : invalid_entry_position
+     valid_entry?(move) ? insert_move(move) : invalid_entry_number
+  end
+
+  def invalid_entry_number
+    display.invalid_number
+    get_move(display.get_player_move(@current_player))
+  end
+
+  def insert_move(position)
+  valid_entry_position?(position) ? board_insertion(position) : invalid_entry_position
   end
 
   def invalid_entry_position
@@ -24,14 +33,15 @@ class Game
     get_move(display.get_player_move(@current_player))
   end
 
-  def insert_move(move)
-    
+  def board_insertion(position)
+    matrix_position = board.get_position_in_matrix(position)
+    @board.insert_At(@current_player, matrix_position.first, matrix_position.last)
+    display.print_board(@board.matrix)
   end
 
-  private
-
-  def valid_entry_position(position)
-    
+  def valid_entry_position?(position)
+    matrix_position = board.get_position_in_matrix(position)
+    board.position_available?(matrix_position.first, matrix_position.last)
   end
 
   def valid_entry?(number)
